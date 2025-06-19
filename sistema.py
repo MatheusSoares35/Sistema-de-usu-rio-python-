652c877c3ccfbc922d3c7032125a0801b79c8bc0
Matheus Santos <matheus.busyness0035@gmail.com>
2025-06-19 12:17:07 -0300

    organização de pasta

---
+++ sistema.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+# Cria a janela principal
+janela = tk.Tk()
+
+# Define o título da janela
+janela.title("Minha Janela Tkinter")
+
+# Define as dimensões da janela (opcional)
+janela.geometry("400x300")  # Largura x Altura
+
+# Inicia o loop principal da janela
+janela.mainloop()
