f2749636c6d80555ef672c44064ee511e6dcf24f
Matheus Santos <matheus.busyness0035@gmail.com>
2025-06-19 18:53:16 -0300

    removeu arquivo sistemas.py

--- sistema.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-janela = Tk()
-janela.title("aqui temos uma janela")
-janela.geometry('420x420')
-label = Label(janela,text="aqui temos um texto",font=('arial',20,'bold'),fg='green')
-label.pack()
-janela.mainloop()
